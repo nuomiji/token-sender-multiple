b4fb6a4a4e8931a2dcc77a53a120b529e3d6f073
Kelly <xiaohandang@gmail.com>
2018-07-19 16:55:15 -0700

    extracted config variables

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {
+	myAddress: "0xA0bEc14CCf8Cb61db70557b07F2703C7c8ce4C69",
+	myPrivateKey: "eb9c47dbe32787bb641048df11ea0ed5ca93f1d40b0343a4be0da7c2713358a6",
+	contractAddress: "0x14466590b32b83BE64898fD8b70E1a050DA0a9D0",
+	inputFileName: "Destinations.csv"
+}
+
+module.exports = config;
--- server.js
+++ server.js
@@ -1,24 +1,20 @@
 var fs = require('fs');
 var parse = require('csv-parse/lib/sync');
+var config = require('./config');
 // var stringigy = require('csv-stringify');
 
 const Tx = require('ethereumjs-tx');
 const Web3 = require('web3');
 var web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/vCfQu4uCspVZEATQTcmJ'));
 
-var myAddress = "0xA0bEc14CCf8Cb61db70557b07F2703C7c8ce4C69";
-var myPrivateKey = "eb9c47dbe32787bb641048df11ea0ed5ca93f1d40b0343a4be0da7c2713358a6";
-var contractAddress = "0x14466590b32b83BE64898fD8b70E1a050DA0a9D0";
-var inputFileName = 'Destinations.csv';
-
 var transactionRecords = [];
 
-var input = readCSV(inputFileName);
+var input = readCSV(config.inputFileName);
 var abiArray = JSON.parse(fs.readFileSync('abi.json', 'utf-8'));
-var contract = web3.eth.contract(abiArray).at(contractAddress);
-var privKey = new Buffer(myPrivateKey, 'hex');
+var contract = web3.eth.contract(abiArray).at(config.contractAddress);
+var privKey = new Buffer(config.myPrivateKey, 'hex');
 
-var transactionCount = web3.eth.getTransactionCount(myAddress);
+var transactionCount = web3.eth.getTransactionCount(config.myAddress);
 
 input.forEach((element) => {
 
@@ -30,7 +26,7 @@ input.forEach((element) => {
         "nonce": web3.toHex(transactionCount),
         "gasPrice": web3.toHex(10),
         "gasLimit": web3.toHex(500000),
-        "to": contractAddress,
+        "to": config.contractAddress,
         "value": web3.toHex(0),
         "data": contract.transfer.getData(toAddress, amount, {from: "myAddress"}),
         "chainId": 0x03
