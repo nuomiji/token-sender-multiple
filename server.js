036906a1b2e2e51b35ba55611f9f7899bcaa5c80
Kelly <xiaohandang@gmail.com>
2018-07-19 15:32:12 -0700

    Version 2
    created transactionsRecords array, but still unable to record hash into the array due to async issues

--- server.js
+++ server.js
@@ -3,44 +3,50 @@ var parse = require('csv-parse/lib/sync');
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var stringigy = require('csv-stringify');
 
 const Tx = require('ethereumjs-tx');
 const Web3 = require('web3');
 var web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/vCfQu4uCspVZEATQTcmJ'));
 
-
-var myAddress = "0xA0bEc14CCf8Cb61db70557b07F2703C7c8ce4C69";
-var myPrivateKey = "eb9c47dbe32787bb641048df11ea0ed5ca93f1d40b0343a4be0da7c2713358a6";
-var count = web3.eth.getTransactionCount(myAddress);
-
 // input is an array of objects
 var input = parse(fs.readFileSync('Destinations.csv', 'utf-8'), {columns: true});
 
-
 app.use(bodyParser.urlencoded({ extended: true }));
 
+var myAddress, myPrivateKey;
+var transactionRecords = [];
+
+
 app.post('/send', (req, res) => {
-    var myAddress = req.body.fromAddress;
-    var myPrivateKey = req.body.fromPrivateKey;
+    myAddress = req.body.fromAddress;
+    myPrivateKey = req.body.fromPrivateKey;
+    var count = web3.eth.getTransactionCount(myAddress);
+    var itemsProcessed = 0;
 
     input.forEach((element) => {
-        sendToken(element.Address, element.Amount);
+        hash = sendToken(element.Address, element.Amount, count);
+        count++;  
     });
+// create a csv record
+// fs.writeFile("./output.csv", stringigy(transactionRecords), (err)=>{
+//     if(err) return console.log(err);
+//     console.log("The file was saved!");
+// });
 
 });
 
-
-function sendToken(toAddress, amount) {
+function sendToken(toAddress, amount, count) {
 
     var abiArray = JSON.parse(fs.readFileSync('abi.json', 'utf-8'));
     var contractAddress = "0x14466590b32b83BE64898fD8b70E1a050DA0a9D0";
     var contract = web3.eth.contract(abiArray).at(contractAddress);
-    
+
     var rawTransaction = {
         "from": "myAddress",
-        "nonce": web3.toHex(count++),
-        "gasPrice": web3.toHex(4),
-        "gasLimit": web3.toHex(100000),
+        "nonce": web3.toHex(count),
+        "gasPrice": web3.toHex(10),
+        "gasLimit": web3.toHex(500000),
         "to": contractAddress,
         "value": web3.toHex(0),
         "data": contract.transfer.getData(toAddress, amount, {from: "myAddress"}),
@@ -56,14 +62,21 @@ function sendToken(toAddress, amount) {
     web3.eth.sendRawTransaction('0x' + serializedTx.toString('hex'), function(err, hash) {
         if (!err){
             console.log(hash);
+
+            let transactionRecord = {
+                Address: toAddress,
+                Amount: amount,
+                TxHash: hash
+            }
+            transactionRecords.push(transactionRecord);
         }
-        else
+        else{
             console.log(err);
+        }
+
     });
 
 };
-
-
 
 var port = 8080;
 app.listen(port, () => console.log("Listening on port " + port));
