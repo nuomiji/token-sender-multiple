dd3db86501ada53e1f3acac6c325ecc867a0637f
Kelly <xiaohandang@gmail.com>
2018-07-19 16:36:39 -0700

    grouped global variables together

--- server.js
+++ server.js
@@ -9,32 +9,35 @@ const Tx = require('ethereumjs-tx');
 const Web3 = require('web3');
 var web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/vCfQu4uCspVZEATQTcmJ'));
 
-// input is an array of objects
-var input = parse(fs.readFileSync('Destinations.csv', 'utf-8'), {columns: true});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var myAddress, myPrivateKey;
+var myAddress = "0xA0bEc14CCf8Cb61db70557b07F2703C7c8ce4C69";
+var myPrivateKey = "eb9c47dbe32787bb641048df11ea0ed5ca93f1d40b0343a4be0da7c2713358a6";
+var contractAddress = "0x14466590b32b83BE64898fD8b70E1a050DA0a9D0";
+var inputFileName = 'Destinations.csv';
+
 var transactionRecords = [];
+
+var input = readCSV(inputFileName);
+var abiArray = JSON.parse(fs.readFileSync('abi.json', 'utf-8'));
+var contract = web3.eth.contract(abiArray).at(contractAddress);
+
+var transactionCount = web3.eth.getTransactionCount(myAddress);
 
 
 app.post('/send', (req, res) => {
-    myAddress = req.body.fromAddress;
-    myPrivateKey = req.body.fromPrivateKey;
-    var count = web3.eth.getTransactionCount(myAddress);
-    var itemsProcessed = 0;
 
     input.forEach((element) => {
-        var abiArray = JSON.parse(fs.readFileSync('abi.json', 'utf-8'));
-        var contractAddress = "0x14466590b32b83BE64898fD8b70E1a050DA0a9D0";
-        var contract = web3.eth.contract(abiArray).at(contractAddress);
+
+        
 
         var toAddress = element.toAddress;
         var amount = element.amount;
 
         var rawTransaction = {
             "from": "myAddress",
-            "nonce": web3.toHex(count),
+            "nonce": web3.toHex(transactionCount),
             "gasPrice": web3.toHex(10),
             "gasLimit": web3.toHex(500000),
             "to": contractAddress,
@@ -68,7 +71,7 @@ app.post('/send', (req, res) => {
         });
 
 
-    count++;
+    transactionCount++;
 
 
 
@@ -131,3 +134,6 @@ console.log(transactionRecords);
 var port = 8080;
 app.listen(port, () => console.log("Listening on port " + port));
 
+function readCSV (fileName) {
+    return parse(fs.readFileSync(fileName, 'utf-8'), {columns: true});
+}
