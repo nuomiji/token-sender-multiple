6976ef56eb106072f1702c7a7511e73978e2b73f
Kelly <xiaohandang@gmail.com>
2018-07-19 17:05:24 -0700

    REFACTOR extracted buildRawTransaction helper

--- config.js
+++ config.js
@@ -2,7 +2,9 @@ var config = {
 	myAddress: "0xA0bEc14CCf8Cb61db70557b07F2703C7c8ce4C69",
 	myPrivateKey: "eb9c47dbe32787bb641048df11ea0ed5ca93f1d40b0343a4be0da7c2713358a6",
 	contractAddress: "0x14466590b32b83BE64898fD8b70E1a050DA0a9D0",
-	inputFileName: "Destinations.csv"
+	inputFileName: "Destinations.csv",
+	gasPrice: 10,
+	gasLimit: 500000,
 }
 
 module.exports = config;
--- server.js
+++ server.js
@@ -9,30 +9,19 @@ var web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/v
 
 var transactionRecords = [];
 
-var input = readCSV(config.inputFileName);
 var abiArray = JSON.parse(fs.readFileSync('abi.json', 'utf-8'));
 var contract = web3.eth.contract(abiArray).at(config.contractAddress);
 var privKey = new Buffer(config.myPrivateKey, 'hex');
-
 var transactionCount = web3.eth.getTransactionCount(config.myAddress);
+
+var input = readCSV(config.inputFileName);
 
 input.forEach((element) => {
 
     var toAddress = element.toAddress;
     var amount = element.amount;
 
-    var rawTransaction = {
-        "from": "myAddress",
-        "nonce": web3.toHex(transactionCount),
-        "gasPrice": web3.toHex(10),
-        "gasLimit": web3.toHex(500000),
-        "to": config.contractAddress,
-        "value": web3.toHex(0),
-        "data": contract.transfer.getData(toAddress, amount, {from: "myAddress"}),
-        "chainId": 0x03
-    };
-
-
+    var rawTransaction = buildRawTransaction(transactionCount, config.gasPrice, config.gasLimit, toAddress, amount);
     var tx = new Tx(rawTransaction);
 
     tx.sign(privKey);
@@ -67,4 +56,17 @@ console.log(transactionRecords);
 
 function readCSV (fileName) {
     return parse(fs.readFileSync(fileName, 'utf-8'), {columns: true});
+}
+
+function buildRawTransaction(nonce, gasPrice, gasLimit, toAddress, amount){
+    return {
+        "from": "myAddress",
+        "nonce": web3.toHex(nonce),
+        "gasPrice": web3.toHex(gasPrice),
+        "gasLimit": web3.toHex(gasLimit),
+        "to": config.contractAddress,
+        "value": web3.toHex(0),
+        "data": contract.transfer.getData(toAddress, amount, {from: "myAddress"}),
+        "chainId": 0x03
+    }
 }
