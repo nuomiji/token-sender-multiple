5fa559a36b782ffe56e855c02dc1a90c9f21ba43
Kelly <xiaohandang@gmail.com>
2018-07-20 17:24:07 -0700

    Switching from Ropsten to Mainnet

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 var config = {
 	myAddress: "0xA0bEc14CCf8Cb61db70557b07F2703C7c8ce4C69",
 	myPrivateKey: "eb9c47dbe32787bb641048df11ea0ed5ca93f1d40b0343a4be0da7c2713358a6",
-	contractAddress: "0x14466590b32b83BE64898fD8b70E1a050DA0a9D0",
-	inputFileName: "./src/Book1.csv",
+	contractAddress: "0xd71EeF0B84E3AFe44eCEC65111368f3D06B4adBA",
+	inputFileName: "./src/Destinations.csv",
 	gasPrice: 10,
 	gasLimit: 500000,
 }
