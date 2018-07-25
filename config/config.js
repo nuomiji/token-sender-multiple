ec6061435b98c44cd77fcbb88b2abd460ee51ed9
Kelly <xiaohandang@gmail.com>
2018-07-25 15:38:15 -0700

    updated sender and contract address, still need to figure out to field of a transaction

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 var config = {
 	myAddress: "0xc2031a3f846497d0e6f03a11a63c2fe2cd2410b7",
-	myPrivateKey: "eb9c47dbe32787bb641048df11ea0ed5ca93f1d40b0343a4be0da7c2713358a6",
-	contractAddress: "0x14466590b32b83be64898fd8b70e1a050da0a9d0",
+	myPrivateKey: "37d064541baa5f3ccbd019a2e29a0391822afcb6c554a15ee889768d93737c0a",
+	contractAddress: "0xfAb30a807457aB90Fdd8588Fd59268Cf120EdB4A",
 	inputFileName: "./src/Destinations.csv",
 	gasPrice: 10,
 	gasLimit: 500000,
--- server.js
+++ server.js
@@ -4,14 +4,16 @@ var parse = require('csv-parse/lib/sync');
 var delay = require('delay');
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 const csvWriter = createCsvWriter({
-  path: './outputs/transactions.csv',
-  header: [
-    {id: 'Name', title: 'Name'},
-    {id: 'Address', title: 'Address'},
-    {id: 'Amount', title: 'Amount'},
-    {id: 'TxHash', title: 'TxHash'},
-  ]
+	path: './outputs/transactions.csv',
+	header: [
+		{id: 'Name', title: 'Name' },
+		{id: 'Address', title: 'Address'},
+		{id: 'Amount',	title: 'Amount' },
+		{id: 'TxHash', title: 'TxHash' },
+	]
 });
+
+
 
 const Tx = require('ethereumjs-tx');
 const Web3 = require('web3');
@@ -21,6 +23,7 @@ var abiArray = JSON.parse(fs.readFileSync('./src/abi.json', 'utf-8'));
 var contract = web3.eth.contract(abiArray).at(config.contractAddress);
 var privKey = new Buffer(config.myPrivateKey, 'hex');
 var transactionCount = web3.eth.getTransactionCount(config.myAddress);
+console.log("transactionCount: " + transactionCount);
 
 
 var input = readCSV(config.inputFileName);
@@ -28,85 +31,96 @@ var transactionRecords = [];
 var arrayCounter = 0;
 
 
-console.log(web3.isConnected());
-// sendTokens(input);
-// writeToCSV();
 
-function readCSV (fileName) {
-  return parse(fs.readFileSync(fileName, 'utf-8'), {columns: true});
+sendTokens(input);
+writeToCSV();
+
+function readCSV(fileName) {
+	return parse(fs.readFileSync(fileName, 'utf-8'), {
+		columns: true
+	});
 }
 
-function buildRawTransaction(nonce, gasPrice, gasLimit, toAddress, amount){
-  return {
-    // "from": "myAddress",
-    "nonce": web3.toHex(nonce),
-    "gasPrice": web3.toHex(gasPrice),
-    "gasLimit": web3.toHex(gasLimit),
-    "to": config.contractAddress,
-    "value": web3.toHex(0),
-    "data": contract.transfer.getData(toAddress, amount, {from: "myAddress"}),
-    "chainId": 0x01
-  }
+function buildRawTransaction(nonce, gasPrice, gasLimit, toAddress, amount) {
+	console.log("Nonce: " + nonce);
+	return {
+		// "from": "myAddress",
+		// "nonce": web3.toHex(nonce),
+		"nonce": nonce,
+		"gasPrice": web3.toHex(gasPrice),
+		"gasLimit": web3.toHex(gasLimit),
+		"to": config.contractAddress,
+		"value": web3.toHex(0),
+		"data": contract.transfer.getData(toAddress, amount, {
+			from: "myAddress"
+		}),
+		"chainId": 999
+	}
 }
 
 function sendToken(serializedTx, toAddress, amount, name) {
 
-  web3.eth.sendRawTransaction('0x' + serializedTx.toString('hex'), function(err, hash) {
-    if (!err){
+	web3.eth.sendRawTransaction('0x' + serializedTx.toString('hex'), function(err, hash) {
+		if (!err) {
 
-      let transactionRecord = {
-        Name: name,
-        Address: toAddress,
-        Amount: amount,
-        TxHash: hash
-      }
+			let transactionRecord = {
+				Name: name,
+				Address: toAddress,
+				Amount: amount,
+				TxHash: hash
+			}
 
-      transactionRecords.push(transactionRecord);
-      console.log(hash);
-      arrayCounter++;
-    }
-    else{
-      console.error(err);
-    }
-  });
+			transactionRecords.push(transactionRecord);
+			console.log(hash);
+			arrayCounter++;
+			var receipt = web3.eth.getTransactionReceipt(hash);
+			console.log("Receipt:" + receipt);
+
+
+		} else {
+			console.error(err);
+		}
+	});
 }
 
-function sendTokens (input){
+function sendTokens(input) {
 
-  for(var i = 0; i < input.length; i++) {
+	for (var i = 0; i < input.length; i++) {
 
-    var element = input[i];
+		var element = input[i];
 
-    var toAddress = element.Address;
-    var amount = element.Amount;
-    var name = element.Name;
+		var toAddress = element.Address;
+		var amount = element.Amount;
+		var name = element.Name;
 
-    var rawTransaction = buildRawTransaction(transactionCount, config.gasPrice, config.gasLimit, toAddress, amount);
-    var tx = new Tx(rawTransaction);
+		var rawTransaction = buildRawTransaction(transactionCount, config.gasPrice, config.gasLimit, toAddress, amount);
+		var tx = new Tx(rawTransaction);
 
-    tx.sign(privKey);
-    var serializedTx = tx.serialize();
+		tx.sign(privKey);
+		var serializedTx = tx.serialize();
 
-    sendToken(serializedTx, toAddress, amount, name);
+		sendToken(serializedTx, toAddress, amount, name);
 
-    transactionCount++;
+		transactionCount++;
 
-  }
+	}
 }
 
-async function writeToCSV(){
+async function writeToCSV() {
 
-  while(arrayCounter !== input.length){
-    await delay(500);
-  }
 
-  buildCSV();
+
+	while (arrayCounter !== input.length) {
+		await delay(500);
+	}
+
+	buildCSV();
 }
 
-function buildCSV (){
-  // create a csv record
-  csvWriter.writeRecords(transactionRecords)
-  .then(() => {
-    console.log('...Done');
-  });
+function buildCSV() {
+	// create a csv record
+	csvWriter.writeRecords(transactionRecords)
+		.then(() => {
+			console.log('...Done');
+		});
 }
